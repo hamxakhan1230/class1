e4e93cdab516941ae6c0f80a96ab296791832a8e
hamxakhan <hamxakhan1230@gmail.com>
2024-01-29 22:01:32 +0500

    "29/01/2024"

---
+++ class1.php
@@ -0,0 +1,20 @@
+<?php
+$a = 26; // int
+$b = 26.60; // float
+$c = "hamza" // string
+$color = [black,blue,green] // array
+
+echo $a.<br>.$b.<br>.$c.<br>.$d.
+<br>
+print_r($color)
+<br>
+
+// DATA TYPE CHECK
+var_dump($color)
+<br>
+// CHARACTER CHECK
+echo strlen($d)
+<br>
+// WORD COUNT
+echo str_word_count($c)
+?>
