fb24326a07e2d71cf3e50b0d156263e0ef3e28e1
hamxakhan <hamxakhan1230@gmail.com>
2024-01-31 22:51:01 +0500

    "operators"

---
+++ operators.php
@@ -0,0 +1,48 @@
+<?php
+// $a = 6;
+// $b = 8;
+ 
+// // arithmatic operator
+// echo $a+$b;
+// echo "<br>";
+
+// echo $a-$b;
+// echo "<br>";
+
+// echo $a*$b;
+// echo "<br>";
+
+// echo $a/$b;
+// echo "<br>";
+
+// echo $a%$b;
+// echo "<br>";
+
+// echo $a++;
+// echo "<br>";
+// echo $a;
+
+// echo $b--;
+// echo "<br>";
+// echo $a;
+
+$a = "28";
+$b = 28;
+
+if($a==$b){
+    echo "is True";
+}else{
+    echo "is False";
+}
+echo "<br>";
+
+$a = "12";
+$b = 12;
+
+if($a===$b){
+    echo "is True";
+}else{
+    echo "is False";
+}
+echo "<br>";
+?>
